1355bf1aeab8487af28b8075f08fb86ec8d26643
CharlietheCharlie <st896218@gmail.com>
2023-03-21 18:56:19 +0800

    3/21 navbar scroll style

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ let beforeScrollTop = document.documentElement.scrollTop;
         scrollPosition= window.scrollY;
         const afterScrollTop = document.documentElement.scrollTop;
         const delta = afterScrollTop - beforeScrollTop;
-        if (delta < 0 && scrollPosition) {
+        if (delta < 0 && scrollPosition > 100) {
             navbar.style.top = "0";
-            navbar.classList.add("hover");
+            navbar.classList.add("nav-scroll");
         } else if (delta > 0 && scrollPosition > 100) {
             navbar.style.top = "-100%";
         } else if(scrollPosition < 100){
-            navbar.classList.remove("hover");
+            navbar.classList.remove("nav-scroll");
         }
             else {
             return;
